d1a05d33c05a071cf9d6a42d64ae39f1e29d991e
Ecaterini <kateyakovenko0@gmail.com>
2025-12-16 19:25:15 +0100

    Refactor: migrate state from React Context to Redux Toolkit

---
+++ src/components/ComponentC.jsx
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "../redux/appSlice";
+
+function ComponentC() {
+  const user = useSelector((state) => state.app.user);
+  const theme = useSelector((state) => state.app.theme);
+
+  const dispatch = useDispatch();
+
+  return (
+    <div style={{ marginLeft: "40px" }}>
+      <h4>Component C</h4>
+      <p>
+        Користувач: <strong>{user}</strong>
+      </p>
+      <p>
+        Поточна тема: <strong>{theme}</strong>
+      </p>
+
+      <button onClick={() => dispatch(toggleTheme())}>Змінити тему</button>
+    </div>
+  );
+}
+
+export default ComponentC;
